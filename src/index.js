b1cb5df11d58b95f11ce89b118fb163364ac03d3
Midomik <krastikrabbs9234@gmail.com>
2023-09-19 16:11:34 +0300

    comm 2

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 import { fetchImg } from "./js/fetchImg";
+import Notiflix from 'notiflix';
 
 const formEl = document.querySelector(".search-form");
 const searchEl = formEl.firstElementChild;
@@ -8,11 +9,10 @@ const loMore = document.querySelector(".load-more");
 
 let page = 1;
 let count = 0;
+const perPage = 40;
 
-
-const renderImages = (arr)=>{
-    
-        
+const renderImages = (arr, maxPages)=>{
+         
     const markup = arr.map(item=>{return `<a class="large-img-link" href="${item.largeImageURL}">
       <div class="photo-card">
         <img class="img" src="${item.webformatURL}" alt="${item.tags}" loading="lazy"width="400" height="250" />
@@ -26,58 +26,56 @@ const renderImages = (arr)=>{
     </a>`
   }).join("")
     galleryEl.insertAdjacentHTML("beforeend", markup);
-    loMore.style.display = "block";
+    // loMore.style.display = "block";
     page+=1;
-    
+    console.log(maxPages);
+    console.log(page);
 
+    if(page>=maxPages){
+        loMore.style.display="none";
+
+        Notiflix.Notify.success(`The end.`);
+    }
 }
 
-const imgHandler = (e)=>{
-    
-    
-    
+const imgHandler = (e)=>{ 
     e.preventDefault();
-    fetchImg(searchEl.value, page).then(item =>{
-        
-        if(searchEl.value===""){
-            console.log("Enter a request");
-            return;
-        }
-
+    
+    if(searchEl.value==="".trim() ){
+        Notiflix.Notify.failure('Enter a request');
+        return;
+    }
+    fetchImg(searchEl.value, page, perPage).then(item =>{
+          
         if (count!==0){
             galleryEl.innerHTML="";
             page=1;
         }
-        
-        if (item.totalHits===0 || item.totalHits===1){
-            
-            console.log("No photo at your request");
+        if (item.totalHits===0 || item.totalHits===1){   
+            Notiflix.Notify.failure('No photo at your request');
             loMore.style.display = "none";
         } else{
             renderImages(item.hits);
-            count=count+1;
             
-           
-        }
-        
-        
+            if(item.total>=40){
+                loMore.style.display = "block";
+            }
+            Notiflix.Notify.success(`Hooray! We found ${item.total} images.`);
+            count=count+1;           
+        }    
     }).catch(err=>{console.log(err)})
-    
-    
 }
+
+
+
 
 const loadMoreImg = () =>{
-    fetchImg(searchEl.value, page).then(item=>{
-        const maxPages = Math.ceil(item.totalHits/40);
-        if(page>maxPages){
-            loMore.style.display="none";
-            console.log("Hooray! We found totalHits images.")
-        }else{
-        renderImages(item.hits);   
-        }
+    fetchImg(searchEl.value, page, perPage).then(item=>{
+        const maxPages = Math.ceil(item.totalHits/perPage);
+        renderImages(item.hits, maxPages);   
+
     })
 }
-
 
 loMore.addEventListener("click", loadMoreImg);
 formEl.addEventListener("submit", imgHandler)
--- src/js/fetchImg.js
+++ src/js/fetchImg.js
@@ -1,16 +1,30 @@
 import { KEY } from "./api-key";
 import axios from "axios";
-const apiKEY = axios.defaults.headers.common["x-api-key"] = KEY;
+import Notiflix from 'notiflix';
 
-export async function fetchImg(query, page){
+// const apiKEY = axios.defaults.headers.common["x-api-key"] = KEY;
+
+axios.defaults.baseURL = 'https://pixabay.com/api/';
+axios.interceptors.response.use(
+    response => {
+      return response;
+    },
+    error => {
+      Notiflix.Notify.failure('Something went wrong! Try later)');
+      return Promise.reject(error);
+    },
+  );
+
+export async function fetchImg(query, page, perPage){
     const searchParams = new URLSearchParams({
         q: `${query}`,
         image_type: "photo",
         orientation: "horizontal",
         safesearch: "true",
         page: `${page}`,
-        per_page: 40,
+        per_page: perPage,
     })
-    const response = (await fetch(`https://pixabay.com/api/?key=${apiKEY}&${searchParams.toString()}`)).json();
-    return response;
+    const response = await axios.get(`?key=${KEY}&${searchParams.toString()}`);
+    console.log(response.data)
+    return response.data;
 }
